607081d30da2bad8c35f702dab2a4b2346625dcc
Aorubommerkun <68483197+Aorubommerkun@users.noreply.github.com>
2020-08-30 15:56:17 +0900

    Update users.js

--- users.js
+++ users.js
@@ -1,3 +1,3 @@
-let s2 = request.open('GET', https://api.scratch.mit.edu/users/apple502j, true);
+let s2 = GET https://api.scratch.mit.edu/users/apple502j;
 let arr2 = s2.split(",");
 console.log(arr2);
